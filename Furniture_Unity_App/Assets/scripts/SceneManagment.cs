34414f17bfe13ca293167f2353d020442738524c
Shariq Ali Khan <38683828+shariqkhanyousufzai@users.noreply.github.com>
2019-10-06 21:35:56 +0500

    Assets
    Assets

---
+++ Furniture_Unity_App/Assets/scripts/SceneManagment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagment : MonoBehaviour
+{
+    public void GotoLandingScene()
+    {
+        SceneManager.LoadScene("landingscreen");
+    }
+
+    public void GotoAboutScene()
+    {
+        SceneManager.LoadScene("aboutus");
+    }
+
+    public void GotoCategoryScene()
+    {
+        SceneManager.LoadScene("category");
+    }
+}
